43c54d13717310dbdfa641ed72af9d7b0b714fd3
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-09-25 16:37:22 -0600

    Workaround for https://github.com/adafruit/Adafruit_CircuitPython_datetime/issues/22

--- code.py
+++ code.py
@@ -28,6 +28,10 @@ class API:
 
     @staticmethod
     def to_datetime(as_str: str) -> adafruit_datetime.datetime:
+        # workaround for https://github.com/adafruit/Adafruit_CircuitPython_datetime/issues/22
+        if as_str[-1] == "Z":
+            print(f"Warning: applying workaround for unsupported datetime format {as_str}")
+            as_str = as_str[:-1] + "-00:00"
         return adafruit_datetime.datetime.fromisoformat(as_str)
 
     def init_requests(self) -> None:
