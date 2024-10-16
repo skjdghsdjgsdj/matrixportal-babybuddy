b5729c7526358d09f42735d3730ed10ac91f2786
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-10-16 12:00:50 -0600

    Workaround for https://github.com/adafruit/Adafruit_CircuitPython_datetime/issues/22

--- external_rtc.py
+++ external_rtc.py
@@ -18,6 +18,22 @@ class ExternalRTC:
         self.device = adafruit_pcf8523.pcf8523.PCF8523(i2c)
         self.rtc_utc_offset = None
 
+    @staticmethod
+    def to_datetime(as_str: str) -> adafruit_datetime.datetime:
+        """
+        A workaround for a CircuitPython bug that fails to parse ISO times that end with "Z":
+        https://github.com/adafruit/Adafruit_CircuitPython_datetime/issues/22
+
+        :param as_str: Date/time as an ISO string
+        :return: adafruit_datetime.datetime.fromisoformat(as_str), but if as_str ends with "Z", replace the "Z" with
+        "-00:00" to emulate UTC and a format that it does parse
+        """
+
+        if as_str[-1] == "Z":
+            print(f"Warning: applying workaround for unsupported datetime format {as_str}")
+            as_str = as_str[:-1] + "-00:00"
+        return adafruit_datetime.datetime.fromisoformat(as_str)
+
     def sync(self, requests: adafruit_requests.Session):
         print("Updating RTC from Adafruit IO")
 
@@ -28,8 +44,12 @@ class ExternalRTC:
             raise ValueError("adafruit.io username or key not defined in settings.toml")
 
         response = requests.get(f"https://io.adafruit.com/api/v2/{username}/integrations/time/clock?x-aio-key={api_key}")
-        now = datetime.fromisoformat(response.text)
+        now = ExternalRTC.to_datetime(response.text)
         self.rtc_utc_offset = (now.utcoffset().seconds / 60 / 60) - 24
+        while self.rtc_utc_offset >= 24:
+            self.rtc_utc_offset -= 24
+        while self.rtc_utc_offset <= -24:
+            self.rtc_utc_offset += 24
 
         print(f"Setting RTC to {now}")
         self.device.datetime = struct_time((
